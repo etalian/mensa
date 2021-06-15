d62527a47eed5cb2751e998c14bb36d18bd03a5d
etalian <io@etalian.me>
2021-06-15 21:18:55 +0200

    Fix test-caravel CI job

--- caravel_test/project.c
+++ caravel_test/project.c
@@ -17,48 +17,81 @@
 
 #include "verilog/dv/caravel/defs.h"
 
-/*
-	IO Test:
-		- Configures MPRJ lower 8-IO pins as outputs
-		- Observes counter value through the MPRJ lower 8 IO pins (in the testbench)
-*/
+#define BASE_ADDR 0x30000000
+#define REG_CTRL  (BASE_ADDR + 0x00)
+#define REG_OP    (BASE_ADDR + 0x04)
+#define REG_A     (BASE_ADDR + 0x08)
+#define REG_B     (BASE_ADDR + 0x0c)
+#define REG_C     (BASE_ADDR + 0x10)
+#define REG_RMODE (BASE_ADDR + 0x14)
+#define REG_OUT   (BASE_ADDR + 0x18)
+#define REG_FLAGS (BASE_ADDR + 0x1C)
+#define REG_COCO  (BASE_ADDR + 0x20)
+
+static uint32_t read(unsigned long addr)
+{
+  return *(volatile uint32_t *)addr;
+}
+
+static void write(unsigned long addr, uint32_t val)
+{
+  *(volatile uint32_t *)addr = val;
+}
 
 void main()
 {
-	/* 
-	IO Control Registers
-	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
-	| 3-bits | 1-bit | 1-bit | 1-bit  | 1-bit  | 1-bit | 1-bit   | 1-bit   | 1-bit | 1-bit | 1-bit   |
+  reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_18 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_17 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_16 = GPIO_MODE_MGMT_STD_OUTPUT;
 
-	Output: 0000_0110_0000_1110  (0x1808) = GPIO_MODE_USER_STD_OUTPUT
-	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
-	| 110    | 0     | 0     | 0      | 0      | 0     | 0       | 1       | 0     | 0     | 0       |
-	
-	 
-	Input: 0000_0001_0000_1111 (0x0402) = GPIO_MODE_USER_STD_INPUT_NOPULL
-	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
-	| 001    | 0     | 0     | 0      | 0      | 0     | 0       | 0       | 0     | 1     | 0       |
+  reg_mprj_io_15 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_14 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_13 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_7  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_5  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_4  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_3  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_2  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_1  = GPIO_MODE_USER_STD_OUTPUT;
+  reg_mprj_io_0  = GPIO_MODE_USER_STD_OUTPUT;
 
-	*/
+  /* Apply configuration */
+  reg_mprj_xfer = 1;
+  while (reg_mprj_xfer == 1);
 
-    // 1 input for input signal
-	reg_mprj_io_8 =   GPIO_MODE_USER_STD_INPUT_NOPULL;
+  // activate the project by setting the 1st bit of 2nd bank of LA - depends on the project ID
+  reg_la1_iena = 0; // input enable off
+  reg_la1_oenb = 0; // output enable on
+  reg_la1_data = 1 << 14;
 
-    // 1 output for segments, starting at 9
-	reg_mprj_io_9 =   GPIO_MODE_USER_STD_OUTPUT;
+  uint32_t val;
+  write(REG_A, 0x40E040C0);
+  write(REG_B, 0x40C040E0);
 
-    /* Apply configuration */
-    reg_mprj_xfer = 1;
-    while (reg_mprj_xfer == 1);
+  val = read(REG_OUT);
 
-    // activate the project by setting the 1st bit of 2nd bank of LA - depends on the project ID
-    reg_la1_iena = 0; // input enable off
-    reg_la1_oenb = 0; // output enable on
-    reg_la1_data = 1 << 14;
-
-    // do something with the logic analyser
-    reg_la0_iena = 0;
-    reg_la0_oenb = 0;
-    reg_la0_data |= 100;
+  if (val == 0x42284228) {
+    write(REG_COCO, 0xC00D); // cocotb pass
+  } else {
+    write(REG_COCO, 0xBAAD); // cocotb fail
+  }
 }
 
