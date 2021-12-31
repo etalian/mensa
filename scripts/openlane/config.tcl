12229062b79623d7f0d75944c8afc0dcffd0e4c5
etalian <io@etalian.me>
2021-12-31 02:22:04 +0100

    Update skywater CI job to mpw4

--- scripts/openlane/config.tcl
+++ scripts/openlane/config.tcl
@@ -1,13 +1,12 @@
 set ::env(DESIGN_NAME) "bfloat16_fma"
 set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/bfloat16_fma/src/*.v]
-set ::env(CLOCK_PERIOD) 0
+set ::env(CLOCK_PERIOD) 100
 set ::env(CLOCK_TREE_SYNTH) 0
 set ::env(CLOCK_PORT) ""
 
 set ::env(GLB_RT_MAX_DIODE_INS_ITERS) 3
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 275 275"
+set ::env(DIE_AREA) "0 0 300 300"
 
-set ::env(PL_TARGET_DENSITY) 0.60
-#default is 0.55
+set ::env(PL_TARGET_DENSITY) 0.5
